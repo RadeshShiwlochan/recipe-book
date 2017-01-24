6b6b0137b2cca46a06dc982e79edda59b6414c4d
radeshshiwlochan <radesh0430@gmail.com>
2017-01-23 23:56:51 -0500

    adds new images, adds shopping-list array

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('Crab Curry', 'Spicy Crab', 'http://www.jehancancook.com/wp-content/uploads/2015/10/Crab-Curry-9-1024x1017.jpg',[]),
-    new Recipe('Shrimp with Garlic Sauce', 'Spicy Shrimp', 'http://recipes.oregonlive.com/system/recipes/images/000/004/020/hero/shrimp.jpg?1471493610',[]),
-    new Recipe('Lobster Salad', 'Lobster with Avocado','http://www.finelobster.com/images/Shrimp-Avodcado-Salad.jpg',[])
+    new Recipe('Shrimp with Garlic Sauce', 'Spicy Shrimp', 'http://damndelicious.net/wp-content/uploads/2014/04/IMG_5191edit.jpg',[]),
+    new Recipe('Lobster Salad', 'Lobster with Avocado','https://s-media-cache-ak0.pinimg.com/originals/d6/46/e0/d646e0bdb17720937697492753bf5cef.jpg',[])
   ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingListAddComponent} from './shopping-list-add.component';
+import {Ingredients} from "../ingredients";
 
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
+
+  items: Ingredients[] = [];
 
   constructor() { }
 
